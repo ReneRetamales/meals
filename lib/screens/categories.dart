4f2db2e0379973ad8b617f59f6f6a0daaec15773
Rene Retamales <rretamales@incolur.local>
2026-02-06 13:39:49 -0300

    Agregando carpetas

---
+++ lib/screens/categories.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Elige tu categoría")),
+      body: GridView(
+        padding: EdgeInsets.all(8),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 15,
+          mainAxisSpacing: 15,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
